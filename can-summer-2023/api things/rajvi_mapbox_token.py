9dfd354a9f84b0b1929737042db23e0687bcf252
Rajvishah12 <rajvis2011@gmail.com>
2023-07-06 21:50:40 +0000

    end of day trying to make map

--- can-summer-2023/api things/rajvi_mapbox_token.py
+++ can-summer-2023/api things/rajvi_mapbox_token.py
@@ -1,7 +1,7 @@
 # Store the mapbox isochrone token
 
 def my_token():
-    token = 'sk.eyJ1IjoicnNoYWgyNzYiLCJhIjoiY2xqcmg4NHNhMGVmaDNmbWljMjk3MHR4OSJ9.5Rs78DrtiVsTr-YuP4bj7Q'
+    token = 'sk.eyJ1IjoicnNoYWgyNzYiLCJhIjoiY2xqcm5mbzMxMGc1ejNwcnlwbXdtYzNvbyJ9.xuh-xfCXdR4tV_aFGFTUPg'
     
     return token
-
+ 
