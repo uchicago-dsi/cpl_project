b66739012b49fc148a9d344075cb17563b5c86c6
abby reynolds <abigail.v.reynolds@gmail.com>
2023-07-06 19:29:14 +0000

    api functionality added

---
+++ can-summer-2023/notebooks/api/mapbox_token.py
@@ -0,0 +1,7 @@
+# Store the mapbox isochrone token
+
+def my_token():
+    token = 'pk.eyJ1IjoiYWJieXJleW5vbGRzODgiLCJhIjoiY2xqZXBiZzB6MjlzZjNlc2NqNm5ycnZuaSJ9.WrtemsZEU3cK8U-LZUDf7Q'
+    
+    return token
+
