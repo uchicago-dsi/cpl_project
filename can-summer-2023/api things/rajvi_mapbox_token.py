b070e91aacea9ea180a14ab773ab9929b1578e13
Rajvishah12 <rajvis2011@gmail.com>
2023-07-06 20:41:58 +0000

    libraries plotted on census map

---
+++ can-summer-2023/api things/rajvi_mapbox_token.py
@@ -0,0 +1,7 @@
+# Store the mapbox isochrone token
+
+def my_token():
+    token = 'sk.eyJ1IjoicnNoYWgyNzYiLCJhIjoiY2xqcmg4NHNhMGVmaDNmbWljMjk3MHR4OSJ9.5Rs78DrtiVsTr-YuP4bj7Q'
+    
+    return token
+
