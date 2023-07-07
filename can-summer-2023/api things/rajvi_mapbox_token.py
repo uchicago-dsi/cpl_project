d1a971af4f888e2ac0dd82ca4a78847dca95e6aa
Rajvishah12 <rajvis2011@gmail.com>
2023-07-07 16:57:18 +0000

    api not working

--- can-summer-2023/api things/rajvi_mapbox_token.py
+++
@@ -1,7 +0,0 @@
-# Store the mapbox isochrone token
-
-def my_token():
-    token = 'sk.eyJ1IjoicnNoYWgyNzYiLCJhIjoiY2xqcm5mbzMxMGc1ejNwcnlwbXdtYzNvbyJ9.xuh-xfCXdR4tV_aFGFTUPg'
-    
-    return token
- 
