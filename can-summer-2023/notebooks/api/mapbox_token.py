99aa555ecfed88202bfe396bc16296983271b7de
Tasnim <Tasneemalqudah7@gmail.com>
2023-07-07 16:14:33 +0000

    MapboxTasnim

---
+++ can-summer-2023/notebooks/api/mapbox_token.py
@@ -0,0 +1,6 @@
+# Store the mapbox isochrone token
+
+def my_token():
+    token = 'pk.eyJ1IjoidGFzbmltcXUiLCJhIjoiY2xqcmp3ZmJxMGR5aTNlcndkaWxobWtnMyJ9.CL0UPae46_yFnA3XELq3yA'
+    
+    return token
