9902b6160dfd8d9b30d6350dff8edd059d4eb575
Tasnim <Tasneemalqudah7@gmail.com>
2023-07-07 22:06:08 +0000

    Pushing my work

--- can-summer-2023/notebooks/api/mapbox_api.py
+++ can-summer-2023/notebooks/api/mapbox_api.py
@@ -53,6 +53,6 @@ class MapboxAPI:
         
         full_json = data_response.json()
         
-        geometry = full_json['features'][0]['geometry']['coordinates'][0] #coordinates of polygon
+        geometry = full_json['features'][0]['geometry'] #coordinates of polygon
         
         return geometry
